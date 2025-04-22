bc63e81b9bb1d46dafe56a22c52225d6f49ea10e
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-04-22 17:56:06 +0200

    feat: comments

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://mongodb-auto.onrender.com', // Base URL for API calls.
+  //see if permissions on mongodb could be affecting get requests
 });
 
 export default api;
