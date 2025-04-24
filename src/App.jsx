5f1d10d65393671ba865211eca5e7b2965e5affa
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-04-24 12:14:42 +0200

    feat: test root url correction on gets only

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     try {
       /* Sends a POST request to
        (backend server) */
-      const response = await api.get('');
+      const response = await api.get('/cars');
       setCars(response.data); //update state with cars
     } catch (error) {
       console.error('Error fetching cars:', error);
